f2c69f71222f09e48a847957041bea0f90b2924b
unknown <dheshnaashri>
2026-06-09 15:00:08 +0530

    calculations

---
+++ src/day43.java
@@ -0,0 +1,24 @@
+void main() {
+    System.out.println(add(8,9));
+    System.out.println(subract(5,6));
+    System.out.println(multiplicatiom(6,6));
+    System.out.println(divison(4,2));
+    System.out.println(modulus(9,3));
+
+
+}
+    public int add (int a,int b){
+        return a + b;
+    }
+     public int subract(int a, int b){
+         return a - b;
+}
+    public int multiplicatiom(int a,int b){
+          return a * b;
+    }
+    public int divison(int a,int b){
+         return a / b;
+}
+    public int modulus(int a, int b){
+         return a % b;
+    }
