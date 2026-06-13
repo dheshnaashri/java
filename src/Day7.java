77404111381118c87f92bab90c58afdc37e91cd8
subasree1034 <subasreee9@gmail.com>
2026-06-13 10:40:45 +0530

    solving problem added method

--- src/Day7.java
+++ src/Day7.java
@@ -41,9 +41,10 @@ public class Day7 {
     }
 }
 */
-
+/*
 class Payment {
 void transfer(int amount) {
+
     System.out.println("Payment: " + amount);
 }
 }
@@ -80,11 +81,75 @@ public class Day7{
         p.transfer(2000);
     }
 }
+*/
+public class Day7Interface {
 
+    interface Employee {
+        void solveProblem();
+        void work();
+    }
 
+    static class BankEmployee implements Employee{
+        String name;
+        String id;
+        int salary;
 
+        BankEmployee(String name, String id, int salary) {
+            this.name = name;
+            this.id = id;
+            this.salary = salary;
+        }
 
+        @Override
+        public void solveProblem() {
+            System.out.println(name + " is solving banking problems");
+        }
 
+        @Override
+        public void work() {
+            System.out.println(name + " is working in the bank");
+        }
+    }
+
+    static class CheckingEmployee implements Employee {
+
+        String name;
+        String id;
+        int salary;
+
+        CheckingEmployee(String name, String id, int salary) {
+            this.name = name;
+            this.id = id;
+            this.salary = salary;
+        }
+
+        @Override
+        public void solveProblem() {
+            System.out.println(name + " is checking customer accounts");
+        }
+
+        @Override
+        public void work() {
+            System.out.println(name + " is performing checking operations");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee emp1 = new BankEmployee("subasree sj", "B101", 50000);
+        Employee emp2 = new CheckingEmployee("joseph", "C102", 45000);
+        System.out.println("--------------------------------");
+        emp1.work();
+        System.out.println();
+        emp1.solveProblem();
+        System.out.println("--------------------------------");
+        emp2.work();
+        System.out.println();
+        emp2.solveProblem();
+        System.out.println("--------------------------------");
+
+    }
+}
 
 
 
